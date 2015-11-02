acb6f3766c770358be6a480ef5518c87935af202
unknown <x15g023@PC504N026.chiba-fjb.ac.jp>
2015-11-02 14:53:47 +0900

    課題5

---
+++ src/jp/ac/chibafjb/x15g023/kd105/X15g023105.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g023.kd105;
+
+public class X15g023105 {
+
+	public static void main(String[] args) {
+		System.out.println("氏名：船橋 太郎(ふなばし たろう)");
+		System.out.println("年齢：24");
+		System.out.println("身長：174");
+
+	}
+
+}
