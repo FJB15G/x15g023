a85fb0244b96006f78d40faa6da324dca36e0265
unknown <x15g023@PC504N026.chiba-fjb.ac.jp>
2015-11-09 13:56:51 +0900

    課題8

---
+++ src/jp/ac/chibafjb/x15g023/kd112/X15g023112.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g023.kd112;
+
+public class X15g023112 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<100;i++)
+		{
+			System.out.println("船橋情報ビジネス専門学校");
+		}
+
+	}
+
+}
