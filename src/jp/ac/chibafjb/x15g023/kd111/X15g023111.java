143f1d6aab0a48c7a9b5d08d44c446055e8d635a
unknown <x15g023@PC504N026.chiba-fjb.ac.jp>
2015-11-09 13:56:26 +0900

    課題7

---
+++ src/jp/ac/chibafjb/x15g023/kd111/X15g023111.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g023.kd111;
+
+public class X15g023111 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("砂川 椋哉");
+		}
+
+	}
+
+}
