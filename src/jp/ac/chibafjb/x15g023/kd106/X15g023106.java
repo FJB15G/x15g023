c2318b7779da7f2fff52016541f2248a85de80ad
unknown <x15g023@PC504N026.chiba-fjb.ac.jp>
2015-11-09 13:46:53 +0900

    課題6

---
+++ src/jp/ac/chibafjb/x15g023/kd106/X15g023106.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g023.kd106;
+
+public class X15g023106 {
+
+	public static void main(String[] args) {
+		System.out.println("   ＿");
+		System.out.println("  ││");
+		System.out.println("┌┘└┐");
+		System.out.println("└┐┌┘");
+		System.out.println("  ││");
+		System.out.println("   ─");
+	}
+
+}
